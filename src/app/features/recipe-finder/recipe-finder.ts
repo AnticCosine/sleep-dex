24e7ae2dd5c971c142729906ddd67a6cf7122cef
Rathan Senthil <S_rathan123@outlook.com>
2026-03-13 01:29:58 +1100

    calling service in component

--- src/app/features/recipe-finder/recipe-finder.ts
+++ src/app/features/recipe-finder/recipe-finder.ts
@@ -2,23 +2,22 @@ import { Component } from '@angular/core';
 import { RecipeCard } from '../../shared/components/recipe-card/recipe-card';
 import { Recipe } from '../../models/recipe.models';
 import { RecipeService } from '../../services/recipe-service';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-finder',
-  imports: [RecipeCard],
+  imports: [RecipeCard, CommonModule],
   templateUrl: './recipe-finder.html',
   styleUrl: './recipe-finder.css',
 })
 export class RecipeFinder {
   
-  recipes: Recipe[] = []
+  recipes$!: Observable<Recipe[]>;
 
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
-    this.recipeService.GetRecipes().subscribe(data => {
-      this.recipes = data;
-    });
-    
+    this.recipes$ = this.recipeService.GetRecipes();
   }
 }
--- src/app/shared/components/recipe-card/recipe-card.ts
+++ src/app/shared/components/recipe-card/recipe-card.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../../models/recipe.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-card.html',
   styleUrl: './recipe-card.css',
 })
-export class RecipeCard {}
+export class RecipeCard {
+
+  @Input() recipe!: Recipe;
+
+}
