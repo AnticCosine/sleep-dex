9066cd6747a55720924597e3e0bb5c11e0651332
Rathan Senthil <S_rathan123@outlook.com>
2026-03-15 01:42:38 +1100

    add filters

--- src/app/features/recipe-finder/recipe-finder.ts
+++ src/app/features/recipe-finder/recipe-finder.ts
@@ -3,21 +3,55 @@ import { RecipeCard } from '../../shared/components/recipe-card/recipe-card';
 import { Recipe } from '../../models/recipe.models';
 import { RecipeService } from '../../services/recipe-service';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { combineLatest, debounceTime, map, Observable, startWith } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-finder',
-  imports: [RecipeCard, CommonModule],
+  imports: [RecipeCard, CommonModule, ReactiveFormsModule],
   templateUrl: './recipe-finder.html',
   styleUrl: './recipe-finder.css',
 })
 export class RecipeFinder {
   
+  searchControl = new FormControl('');
+  RecipeTypeControl = new FormControl<string[]>(['curry', 'salad', 'dessert']);
   recipes$!: Observable<Recipe[]>;
+  filteredRecipes$!: Observable<Recipe[]>;
 
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     this.recipes$ = this.recipeService.GetRecipes();
+    
+    this.filteredRecipes$ = combineLatest([
+      this.recipes$,
+      this.searchControl.valueChanges.pipe(startWith(''), debounceTime(100)),
+      this.RecipeTypeControl.valueChanges.pipe(startWith(this.RecipeTypeControl.value ?? []))
+    ]).pipe(
+      map(([recipes, search, recipeTypes]) => {
+
+        const searchTerm = (search ?? ``).toLowerCase();
+
+        return recipes.filter(recipe => {
+
+          const matchedSearch = recipe.name.toLowerCase().includes(searchTerm);
+          const matchedRecipeType = !recipeTypes?.length || recipeTypes.includes(recipe.type);
+
+          return matchedSearch && matchedRecipeType;
+        }
+        );
+      })
+    );
+  }
+
+  toggleRecipeType(type: string, checked: boolean) {
+    const current = this.RecipeTypeControl.value ?? [];
+
+    if (checked) {
+      this.RecipeTypeControl.setValue([...current, type]);
+    } else {
+      this.RecipeTypeControl.setValue(current.filter(t => t !== type));
+    }
   }
 }
