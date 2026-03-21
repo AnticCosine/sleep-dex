172006dfaf608275c262d7ba900aefe532492a14
Rathan Senthil <S_rathan123@outlook.com>
2026-03-22 01:01:17 +1100

    footer component

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,9 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  feedbackText = '';
+  submitted = false;
+ 
+  sendFeedback() {
+    if (!this.feedbackText.trim()) return;
+    
+    console.log('Feedback submitted:', this.feedbackText);
+    this.submitted = true;
+    this.feedbackText = '';
+    setTimeout(() => (this.submitted = false), 3000);
+  }
+}
