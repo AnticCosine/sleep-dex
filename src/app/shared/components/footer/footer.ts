8d8c8a1394124ab0f598cd544a96d175f86fb5fc
Rathan Senthil <S_rathan123@outlook.com>
2026-03-23 23:28:38 +1100

    implemented feedback form

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
@@ -12,13 +14,36 @@ import { RouterModule } from '@angular/router';
 export class Footer {
   feedbackText = '';
   submitted = false;
+  error = false;
+  sending = false;
+ 
+  private readonly API = environment.apiUrl;
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
  
   sendFeedback() {
     if (!this.feedbackText.trim()) return;
+
+    this.sending = true;
+    this.cdr.detectChanges();
     
-    console.log('Feedback submitted:', this.feedbackText);
-    this.submitted = true;
-    this.feedbackText = '';
-    setTimeout(() => (this.submitted = false), 3000);
+    this.http
+      .post(`${this.API}/feedback`, { message: this.feedbackText })
+      .subscribe({
+        next: () => {
+          this.submitted = true;
+          this.error = false;
+          this.sending = false;
+          this.feedbackText = '';
+          this.cdr.detectChanges();
+          setTimeout(() => (this.submitted = false), 3000);
+        },
+        error: () => {
+          this.error = true;
+          this.sending = false;
+          this.cdr.detectChanges();
+          setTimeout(() => (this.error = false), 3000);
+        },
+    });
   }
 }
